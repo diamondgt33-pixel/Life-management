ed610960bc5f9bfd396bb813bf2bf5f48b38f212
diamondgt33-pixel <diamondgt33@gmail.com>
2026-07-22 08:27:55 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -49,3 +49,16 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
+// Tapping the "driving in progress" notification brings the app to the front
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  event.waitUntil(
+    self.clients.matchAll({ type: "window", includeUncontrolled: true }).then((clientList) => {
+      for (const client of clientList) {
+        if ("focus" in client) return client.focus();
+      }
+      if (self.clients.openWindow) return self.clients.openWindow("./");
+    })
+  );
+});
